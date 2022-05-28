2772db2479f5ecfbf7b1d2a046891e6a9e31fbd3
unknown <danieleastwood4@gmail.com>
2022-05-28 12:43:53 -0400

    elements added

---
+++ client/elements/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className=" text-4xl sm:text-2xl  text-[#3C5233]">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Home from "../elements/Home";
+import Register from "../elements/Register";
 
 function App() {
-  const Home = () => {
-    return (
-      <div className=" text-4xl sm:text-2xl  text-[#3C5233]">
-        <h1>Hello World</h1>
-      </div>
-    );
-  };
   return (
     <div>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
