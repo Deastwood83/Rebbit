f232d0efa18630d3f0a8a0026972d01247b53385
“AnomolyAvery” <ashawsolutions@gmail.com>
2022-05-28 13:55:00 -0400

    Fixed client file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState } from 'react';
+import logo from './logo.svg';
 
-import { Route, Routes } from "react-router-dom";
-import Home from "../elements/Home";
-import Register from "../elements/Register";
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './screens/Home';
+import Register from './screens/Register';
 
 function App() {
-  return (
-    <div>
-     <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
