0055ac49824d6377e67bf1e40aa38beaf657f69d
unknown <danieleastwood4@gmail.com>
2022-05-30 22:31:06 -0400

    email type to text

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
                 </label>
                 <input
                   class="inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-indigo-900 bg-white shadow border-2 border-indigo-900 rounded"
-                  type="email"
+                  type="text"
                   placeholder="Username"
                 />
               </div>
