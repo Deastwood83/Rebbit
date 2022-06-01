62e4bb9a64d03346d23ffb7d77aeae9cc16991bb
Austin bambino <link9215@gmail.com>
2022-06-01 19:09:14 -0400

    passweord in register no longer possible

--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Register() {
     return (
        
-        <div class="h-screen bg-gradient-to-br from-emerald-800 to-green-800 flex justify-center items-center w-full">
+        <div class="h-screen bg-gradient-to-br from-emerald-800 to-green-700   flex justify-center items-center w-full">
           <form>
             <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
               <div class="space-y-4">
@@ -18,10 +18,11 @@ function Register() {
                 </div>
                 <div>
                   <label for="email" class="block mb-1 text-gray-600 font-semibold">Password</label>
-                  <input type="text" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
+                  <input type="password"
+                  placeholder="**********" class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" />
                 </div>
               </div>
-              <button class="mt-4 w-full bg-emerald-800  text-indigo-100 py-2 rounded-md text-lg tracking-wide">Register</button>
+              <button class="mt-4 w-full bg-gradient-to-tr from-emerald-800  text-indigo-100 py-2 rounded-md text-lg tracking-wide">Register</button>
             </div>
           </form>
         </div>
