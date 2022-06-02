2be95cf35049c0d1d59340d9c4e09077666b2c82
TylerIsMoore <tylernmoore07@gmail.com>
2022-06-01 20:50:29 -0400

    users and store files created

---
+++ client/src/lib/store/reducers/users.js
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    isLoading: false,
+    users: null,
+
+}
+
+const userSlice = createSlice({
+    name: "users",
+    initialState,
+})
+export default userSlice.reducer
