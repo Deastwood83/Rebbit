8a837c7380767ae05f17c73d79d2ad6950431d01
AnomolyAvery <ashawsolutions@gmail.com>
2022-06-01 21:10:58 -0400

    Removed old auth provider

--- client/src/lib/store/store.js
+++ client/src/lib/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import users from './reducers/users';
+import auth from './reducers/auth';
 
 const store = configureStore({
     reducer: {
         users,
+        auth,
     },
 });
 
