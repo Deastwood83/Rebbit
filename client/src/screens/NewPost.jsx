4732638fe837dbd9860b9c79689ca48c023761ca
moises lozano <moiseslozano1@hotmail.com>
2022-06-06 20:02:59 -0400

    small changes to new post page

--- client/src/screens/NewPost.jsx
+++ client/src/screens/NewPost.jsx
@@ -4,18 +4,22 @@ import AppLayout from '../components/AppLayout';
 const NewPost = () => {
     return (
         <AppLayout>
-            <div className="container mx-auto max-w-5xl">
-                <form className="flex flex-col">
+            <div className="container mx-auto max-w-[400px] bg-emerald-800 ">
+                <form method= 'POST' action= '' className="flex flex-col ">
                     <div>
-                        <label>Title</label>
-                        <input type="text" />
+                        <label class= 'text-white'> Title <span class= 'text-red-500'> * </span> </label>
+                        <input type="text"/>
                     </div>
 
                     <div>
-                        <label>Content</label>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <label class= 'text-white '> Content </label>
+                        <textarea class= '' name="" id="" cols="30" rows="5"></textarea>
                     </div>
+                    
+                    <button type='Submit' class= 'p-3 bg-emerald-800 text-white hover:bg-orange-800'> Submit </button>
+                
                 </form>
+                
             </div>
         </AppLayout>
     );
