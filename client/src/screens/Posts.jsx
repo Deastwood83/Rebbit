864dc144f88cef2c1aa1cee0eed37ffbb9bf43b2
moises lozano <moiseslozano1@hotmail.com>
2022-06-08 20:51:57 -0400

    Posts

---
+++ client/src/screens/Posts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+import posts from "../lib/store/reducers/posts";
+
+const Posts = () => {
+    return (
+    <AppLayout>
+        <div className="bg-emerald-800 ">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-center box-content bg-gray-300 mt-2 text-4xl mb-2 h-16 w-240 border-4 border-b-indigo-500">hiugadfgfdbfgrzsgdfgffzgsrfgfdgdsgfdfdsgzdffhdfzgzfsds </h1>
+                    
+                    <p>fehife</p>
+                </div>  
+        </div>
+    </AppLayout> 
+)}
+
+export default Posts 
