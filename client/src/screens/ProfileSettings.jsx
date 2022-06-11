046bb8a8d3efb0017b5ead7a3a474943cc255b90
TylerIsMoore <tylernmoore07@gmail.com>
2022-06-11 10:08:41 -0400

    Removed "Add Birthday" from Profile Settings

--- client/src/screens/ProfileSettings.jsx
+++ client/src/screens/ProfileSettings.jsx
@@ -50,11 +50,6 @@ const ProfileSettings = () => {
                 className="bg-gray-100 px-4 py-2 outline-none rounded-md w-full"
                 placeholder="My Bio"
               />
-              <h3 className="text-2 font-arial">Add Birthday</h3>
-              <input
-                type="date"
-                className="bg-gray-100 px-4 py-2 outline-none rounded-md w-full"
-                />
             </div>
           </div>
         </div>
