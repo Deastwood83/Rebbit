0aa7154c99adcfaf973b55a6daac6fedd59f6012
“AnomolyAvery” <ashawsolutions@gmail.com>
2022-06-11 12:59:01 -0400

    Updated format and unused imports

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,101 +1,103 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { loginAsync } from "../lib/store/reducers/auth";
-import Logo from "../rebbitLogo.png";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { loginAsync } from '../lib/store/reducers/auth';
+import Logo from '../rebbitLogo.png';
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const dispatch = useDispatch();
-  const { isAuthenticated, error } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const { isAuthenticated, error } = useSelector((state) => state.auth);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/");
-    }
-  }, [isAuthenticated]);
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/');
+        }
+    }, [isAuthenticated]);
 
-  const onLogin = (e) => {
-    e.preventDefault();
-    if (!password || !username) {
-      return;
-    }
+    const onLogin = (e) => {
+        e.preventDefault();
+        if (!password || !username) {
+            return;
+        }
 
-    dispatch(
-      loginAsync({
-        username,
-        password,
-      })
-    );
-  };
+        dispatch(
+            loginAsync({
+                username,
+                password,
+            })
+        );
+    };
 
-  return (
-
-<div className="h-screen bg-gradient-to-br from-emerald-800 to-green-700 flex justify-center items-center w-full">
-          <div className="inline-flex items-center">
-        <img
-          src="https://cdn.discordapp.com/attachments/979149570992918538/981002974593703961/RebbitLogo.png"
-          class="mx-auto"
-        />
-        <span className="text-5xl text-white font-bold mr-10">Rebbit</span>
-      </div>
-          <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
-          <div class="space-y-4">
-            <h1 class="text-center text-2xl font-semibold text-gray-600">
-              Sign In
-            </h1>
-              {error && (
-              <div>
-                <p className="text-red-500 text-center">{error}</p>
-              </div>
-            )}
-            
-            <div>
-              <label htmlFor="" Username class="block mb-1 text-gray-600 font-semibold">
-               
-              </label>
-              <input
-                type="text"
-                class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-		placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-           
-            <div>
-            <label className="block mb-2 font-extrabold" htmlFor="">
-                  Password
-                </label>
-                <input
-                  className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-                  type="password"
-                  placeholder="**********"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
+    return (
+        <div className="h-screen bg-gradient-to-br from-emerald-800 to-green-700 flex justify-center items-center w-full">
+            <div className="inline-flex items-center">
+                <img
+                    src="https://cdn.discordapp.com/attachments/979149570992918538/981002974593703961/RebbitLogo.png"
+                    class="mx-auto"
                 />
+                <span className="text-5xl text-white font-bold mr-10">
+                    Rebbit
+                </span>
             </div>
-         </div>
-              <button className="inline-block w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-yellow-300 font-extrabold bg-green-700 hover:bg-yellow-300 hover:text-green-700 border-3 border-indigo-900 shadow rounded transition duration-200">
-                Sign in
-              </button>
-              <p className="text-center font-extrabold">
-                Don&rsquo;t have an account?{" "}
-                <Link
-                  to={"/register"}
-                  className="text-yellow-400 hover:underline"
-                >
-                  Sign up
-                </Link>
-              </p>
-          </div>
+            <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
+                <div class="space-y-4">
+                    <h1 class="text-center text-2xl font-semibold text-gray-600">
+                        Sign In
+                    </h1>
+                    {error && (
+                        <div>
+                            <p className="text-red-500 text-center">{error}</p>
+                        </div>
+                    )}
+
+                    <div>
+                        <label
+                            htmlFor=""
+                            Username
+                            class="block mb-1 text-gray-600 font-semibold"
+                        ></label>
+                        <input
+                            type="text"
+                            class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 font-extrabold" htmlFor="">
+                            Password
+                        </label>
+                        <input
+                            className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
+                            type="password"
+                            placeholder="**********"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    </div>
+                </div>
+                <button className="inline-block w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-yellow-300 font-extrabold bg-green-700 hover:bg-yellow-300 hover:text-green-700 border-3 border-indigo-900 shadow rounded transition duration-200">
+                    Sign in
+                </button>
+                <p className="text-center font-extrabold">
+                    Don&rsquo;t have an account?{' '}
+                    <Link
+                        to={'/register'}
+                        className="text-yellow-400 hover:underline"
+                    >
+                        Sign up
+                    </Link>
+                </p>
+            </div>
         </div>
-   
-  );
+    );
 }
 
 export default Login;
--- client/src/screens/UserDetails.jsx
+++ client/src/screens/UserDetails.jsx
@@ -1,82 +1,80 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import AppLayout from "../components/AppLayout";
-import { getGravatarUrl } from "../lib/utils/gravatar";
-import Post from "../components/PostCard";
-import { fetchPostsAsync } from '../lib/store/reducers/posts';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import AppLayout from '../components/AppLayout';
+import { getGravatarUrl } from '../lib/utils/gravatar';
 import { useDispatch, useSelector } from 'react-redux';
-import PostCard from "../components/PostCard";
-import { fetchUserByUsernameAsync } from "../lib/store/reducers/users";
+import { fetchUserByUsernameAsync } from '../lib/store/reducers/users';
 
 const UserDetails = () => {
-  const params = useParams();
+    const params = useParams();
 
-  const { user } = useSelector((state) => state.users);
+    const { user } = useSelector((state) => state.users);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const username = params.username;
+    const username = params.username;
 
-  useEffect(() => {
-    dispatch(fetchUserByUsernameAsync(username));
-  }, [username]);
+    useEffect(() => {
+        dispatch(fetchUserByUsernameAsync(username));
+    }, [username]);
 
-  return (
-    <AppLayout>
-      <div>
-        <div>
-          <div
-            className="h-32 w-full rounded-lg object-cover lg:h-48 bg-gradient-to-r from-emerald-500 to-green-400"
-            alt=""
-          />
-        </div>
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-            <div className="flex">
-              <img
-                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
-                src={getGravatarUrl(user?.email ?? 'test@test.com')}
-                alt=""
-              />
-            </div>
-            <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-              <div className="sm:hidden md:block mt-4 min-w-0 flex-1">
-                <h1 className="text-2xl font-bold text-gray-900 truncate">
-                  {user?.username}
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="hidden sm:block md:hidden mt-4 min-w-0 flex-1">
-            <h1 className="text-2xl font-bold text-gray-900 truncate">
-              {user?.username}
-            </h1>
-          </div>
-          <div className=" rounded-b-lg p-5 pt-15 flex flex-col">
-            <div className="flex bg-gray-200 w-20 items-center rounded">
-              {new Date(user?.createdAt).toLocaleDateString() }
-            </div>
-            <div className="flex bg-gray-200">
-              {user?.biography}
-            </div>
-          </div>
+    return (
+        <AppLayout>
+            <div>
+                <div>
+                    <div
+                        className="h-32 w-full rounded-lg object-cover lg:h-48 bg-gradient-to-r from-emerald-500 to-green-400"
+                        alt=""
+                    />
+                </div>
+                <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                        <div className="flex">
+                            <img
+                                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                                src={getGravatarUrl(
+                                    user?.email ?? 'test@test.com'
+                                )}
+                                alt=""
+                            />
+                        </div>
+                        <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                            <div className="sm:hidden md:block mt-4 min-w-0 flex-1">
+                                <h1 className="text-2xl font-bold text-gray-900 truncate">
+                                    {user?.username}
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hidden sm:block md:hidden mt-4 min-w-0 flex-1">
+                        <h1 className="text-2xl font-bold text-gray-900 truncate">
+                            {user?.username}
+                        </h1>
+                    </div>
+                    <div className=" rounded-b-lg p-5 pt-15 flex flex-col">
+                        <div className="flex bg-gray-200 w-20 items-center rounded">
+                            {new Date(user?.createdAt).toLocaleDateString()}
+                        </div>
+                        <div className="flex bg-gray-200">
+                            {user?.biography}
+                        </div>
+                    </div>
 
-          <div className="pt-20 break-all max-width-max flex flex-wrap justify-between">
-          <div className="flex flex-col justify-between">
-                    {/* {loading === 'pending' && (
+                    <div className="pt-20 break-all max-width-max flex flex-wrap justify-between">
+                        <div className="flex flex-col justify-between">
+                            {/* {loading === 'pending' && (
                         <div className="text-center">
                             <h1>Loading...</h1>
                         </div>
                     )}
 
                     {posts && posts.map((post) => <PostCard post={post} />)} */}
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
-        </div>
-    
-    </AppLayout>
-  );
+        </AppLayout>
+    );
 };
 
 export default UserDetails;
