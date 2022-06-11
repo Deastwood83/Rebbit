1d4f9abb3f173f0bca09271403f4d0f784aa69c7
“AnomolyAvery” <ashawsolutions@gmail.com>
2022-06-11 11:03:08 -0400

    Added timestamps to user

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,23 +1,28 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        birthday: Date,
+        biography: String,
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    birthday: Date,
-    biography: String,
-});
+    {
+        timestamps: true,
+    }
+);
 
 const UserModel = model('User', userSchema);
 
