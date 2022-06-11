a6d8a5422d5324dcebd1eb7be98e897402baa0f4
“AnomolyAvery” <ashawsolutions@gmail.com>
2022-06-11 09:58:59 -0400

    Updated post card to link to post page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function PostCard({ post }) {
     return (
-        <div>
+        <Link to={`/posts/${post?._id}`}>
             <div className="containter px-16">
                 <div className="bg-white p-8 rounded-lg shadow-lg relative hover:shadow-2xl transition duration-500">
                     <h1 className="text-2xl text-gray-800 font-semibold mb-3">
-                        {post.title}
+                        {post?.title}
                     </h1>
                     <p className="text-gray-600 leading-6 tracking-normal">
-                        {post.content}
+                        {post?.content}
                     </p>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
